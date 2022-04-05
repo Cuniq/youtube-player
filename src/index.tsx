b0e430e55513625f811447258e2139a5af381f60
Argyroudis Athanasios <argyroudisth@gmail.com>
2022-04-05 20:55:57 +0300

    Add a falsy feeling of safety (Added .env)

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
+console.log(`${process.env.REACT_APP_API_KEY}`);
+
+const rootContainer = ReactDOM.createRoot(document.getElementById("root")!);
+rootContainer.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
